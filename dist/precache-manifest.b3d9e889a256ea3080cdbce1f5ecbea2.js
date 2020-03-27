d43a3644540714fad5302bbf364cee654697f74b
Mike-Damato <damatomike@gmail.com>
2020-03-27 12:32:17 -0400

    Feature - Add Modal on SingleRecipe component to show instructions and add to favorites

---
+++ dist/precache-manifest.b3d9e889a256ea3080cdbce1f5ecbea2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52f1597746e5eb6303f91944f4220166",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac413feb4dbfa587bd03",
+    "url": "/static/css/main.a585f4ae.chunk.css"
+  },
+  {
+    "revision": "1813f93053a2ac916210",
+    "url": "/static/js/2.8ab03529.chunk.js"
+  },
+  {
+    "revision": "ac413feb4dbfa587bd03",
+    "url": "/static/js/main.6fbcbaa8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
