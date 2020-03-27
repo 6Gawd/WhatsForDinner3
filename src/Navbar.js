c54473b35d9a4e6a5f8907ace9f52f1fe86fdda9
Mike-Damato <damatomike@gmail.com>
2020-03-27 17:29:29 -0400

    Add- Added instructions toasts to List page

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,43 +3,65 @@ import { Link } from 'react-router-dom';
 import { AuthContext } from './Auth';
 
 const Navbar = () => {
-	const { currentUser } = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
-	return (
-		<nav className="nav-wrapper indigo">
-			<div className="container">
-				{currentUser ? (
-					<div>
-						<ul>
-							<li>
-								<Link to="/list">List</Link>
-							</li>
-							<li>
-								<Link to="/recipes">Recipes</Link>
-							</li>
-							<li>
-								<Link to="/profile">Profile</Link>
-							</li>
-							<li>
-								<Link to="/favoriterecipes">Favorite Recipes</Link>
-							</li>
-						</ul>
-					</div>
-				) : (
-					<div>
-						<ul>
-							<li>
-								<Link to="/login">Login</Link>
-							</li>
-							<li>
-								<Link to="/signup">Sign Up</Link>
-							</li>
-						</ul>
-					</div>
-				)}
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="nav-wrapper indigo">
+      <div className="container">
+        {currentUser ? (
+          <ul>
+            {/* <li>
+                <Link to="/list">
+                  <img
+                    // className="valign-center"
+                    style={{ height: '40px', width: '40px', margin: '10px' }}
+                    src="/shopping-bag.svg"
+                    alt="What's For Dinner Logo"
+                  />
+                </Link>
+              </li> */}
+            <li>
+              <Link to="/list">List</Link>
+            </li>
+            <li>
+              <Link to="/recipes">Recipes</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/favoriterecipes">Favorite Recipes</Link>
+            </li>
+            <li className="right">
+              <Link to="/">What's For Dinner?</Link>
+            </li>
+            <li className="right">
+              <img
+                style={{
+                  height: '40px',
+                  width: '40px',
+                  margin: '10px'
+                }}
+                src="/shopping-bag.svg"
+                alt="What's For Dinner Logo"
+              />
+            </li>
+          </ul>
+        ) : (
+          <div>
+            <ul>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/ToastNotifications/Toasts.js
+++ src/ToastNotifications/Toasts.js
@@ -1,6 +1,84 @@
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+export const initTrevorToast = () => {
+  toast.info(
+    'Test out these commands: To activate our assistant say, "Hey Trevor!"',
+    {
+      position: 'bottom-left',
+      autoClose: false,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true
+    }
+  );
+};
+
+export const instructionsToast = () => {
+  toast.info(
+    'You can add any food item you like to your list. "Say add Cheese"',
+    {
+      position: 'bottom-left',
+      autoClose: false,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true
+    }
+  );
+  toast.info(
+    'You can also delete any food item off of your list. Say "delete Cheese"',
+    {
+      position: 'bottom-left',
+      autoClose: false,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true
+    }
+  );
+};
+
+export const deleteInstructionsToast = () => {
+  toast.info(
+    'If you want to clear your current shopping list, say "clear my list"',
+    {
+      position: 'bottom-left',
+      autoClose: false,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true
+    }
+  );
+};
+
+export const getRecipesToast = () => {
+  toast.info(
+    'If you want to get some recipes using your current shopping list, say "get recipes"',
+    {
+      position: 'bottom-left',
+      autoClose: false,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true
+    }
+  );
+  toast.info(
+    'If you want to go to your favorite recipes, say "get favorite recipes"',
+    {
+      position: 'bottom-left',
+      autoClose: false,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true
+    }
+  );
+};
+
 export const addIngredientToast = () => {
   toast.success('Added to list', {
     position: 'bottom-center',
@@ -54,4 +132,50 @@ export const removeFromFavoritesToast = () => {
     pauseOnHover: true,
     draggable: true
   });
+};
+
+export const readyToBeginToast = () => {
+  toast.info('say "Yes I am" to begin!', {
+    position: 'top-right',
+    autoClose: 10000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    draggable: true
+  });
+};
+
+export const repeatCurrentStepsToast = () => {
+  toast.info(
+    'You can make Trevor repeat current step - " repeat current step"',
+    {
+      position: 'top-right',
+      autoClose: 10000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      draggable: true
+    }
+  );
+};
+
+export const nextStepsToast = () => {
+  toast.info('You can make Trevor say the next step - "go to next step"', {
+    position: 'top-right',
+    autoClose: 10000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    draggable: true
+  });
+};
+
+export const previousStepsToast = () => {
+  toast.success(
+    'You can make Trevor say the previous step - "go to previous step"',
+    {
+      position: 'top-right',
+      autoClose: 10000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      draggable: true
+    }
+  );
 };
