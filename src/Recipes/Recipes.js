319bce4a253373bbc62a334152f95f32a50befd4
Leslie <lesliew1993@gmail.com>
2020-03-28 16:26:19 -0400

    Feature - User functionality to add favorite recipes with Speech.

--- src/Recipes/Recipes.js
+++ src/Recipes/Recipes.js
@@ -16,17 +16,17 @@ const Recipes = ({ history }) => {
   const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    annyang.start();
-    annyang.addCommands(navbarCommands());
 
     getIngredients().then(ingredients=>{
-    if (ingredients.length > 0) {
-      trevor.text = `getting your recipes`;
-      speechSynth.speak(trevor);
-    } else {
-      trevor.text = `add some ingredients first`;
-      speechSynth.speak(trevor);
-    }})
+      if (ingredients.length > 0) {
+        trevor.text = `getting your recipes`;
+        speechSynth.speak(trevor);
+      } else {
+        trevor.text = `add some ingredients first`;
+        speechSynth.speak(trevor);
+      }})
+      annyang.start();
+      annyang.addCommands(navbarCommands());
 
     return () => {
       annyang.removeCommands();
@@ -103,8 +103,60 @@ const Recipes = ({ history }) => {
       'get recipes': () => {
         history.push("/recipes")
       },
+      'bookmark recipe number *tag': (tag)=>{
+        addRecipeToFavorite(tag-1)
+        console.log('this is the Tag', tag)
+      }
   	}
   }
+
+  const addRecipeToFavorite = async (idx) => {
+
+    const ingre = [];
+      await db
+        .collection('ingredients')
+        .where('userId', '==', currentUser.uid)
+        .get()
+        .then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            const item = doc.data();
+            item.id = doc.id;
+            ingre.push(item);
+          });
+        });
+        const { data } = await axios.get(
+          recipeURLStart +
+            ingre.map(ingredient => ingredient.name).join(',+') +
+            recipeURLEnd
+        );
+        // const addToFav = data[idx]
+
+        const newRecipes = await data.map(recipe => {
+          delete recipe.imageType;
+          delete recipe.usedIngredientCount;
+          delete recipe.missedIngredientCount;
+          recipe.spoonacularId = recipe.id
+          delete recipe.id
+          recipe.missedIngredients = recipe.missedIngredients.map(
+            ingredient => ingredient.name
+          );
+          recipe.usedIngredients = recipe.usedIngredients.map(
+            ingredient => ingredient.name
+          );
+          recipe.ingredients = recipe.missedIngredients.concat(recipe.usedIngredients)
+          delete recipe.unusedIngredients
+          delete recipe.usedIngredients
+          delete recipe.missedIngredients
+          return recipe;
+        });
+        console.log("new recipes", newRecipes)
+
+      const spoon = await axios.get('https://api.spoonacular.com/recipes/' + newRecipes[idx].spoonacularId + '/analyzedInstructions?apiKey=9dbfb748dfa44db2becd40388c22f59c');
+      console.log("spoon", spoon)
+      newRecipes[idx].steps = spoon.data[0].steps.map(step => step.step);
+
+    await db.collection('favoriteRecipes').add(newRecipes[idx]);
+  };
 
   return (
     <div className="col s12 l12">
